be54b537199502a4f2a278991a60bb91231f0b84
ZanaHoxha <zanahoxha681@gmail.com>
2025-03-24 19:48:15 +0100

    module28

---
+++ Module 28/events3.js
@@ -0,0 +1,6 @@
+var button1=document.getElementById("btn1");
+button1.addEventListener('click', function(){
+    alert('hello');
+});
+ourElement.addEventListener('click', function1)
+ourElement.addEventListener('click', function2)
---
+++ Module 28/ushtrime.js
@@ -0,0 +1,19 @@
+
+var button2=document.getElementById("btn2");
+
+
+button2.onmouseover = function(){
+    alert('zana');
+}
+
+var button3=document.getElementById("btn3");
+
+button3.onmouseleave = function(){
+    alert('era');
+}
+
+var button4=document.getElementById("btn4");
+
+button4.onmousewheel = function(){
+    alert('greta');
+}
