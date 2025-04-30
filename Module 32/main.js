ac80b24c35b206515506fc85e87e87c8aeac3dd6
ZanaHoxha <zanahoxha681@gmail.com>
2025-04-30 18:51:04 +0200

    ehgyrthjryjnry

---
+++ Module 32/main.js
@@ -0,0 +1,11 @@
+/*var input = document.getElementById('input_id');    
+var button = document.getElementById('btn_id');
+var text = document.getElementById('teksti_id');
+
+button.onclick=function(){
+    text.innerHTML=input.value;
+}*/
+
+var text="the best school in the world is digital school!";
+var result=text.search("digital school");
+document.getElementById("result1").innerHTML=result;
