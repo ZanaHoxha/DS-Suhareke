8d8d62add231280a900a81cca1075e4f46e9269f
ZanaHoxha <zanahoxha681@gmail.com>
2025-05-05 18:25:10 +0200

    Update main.js

--- Module 32/main.js
+++ Module 32/main.js
@@ -1,11 +1,51 @@
-/*var input = document.getElementById('input_id');    
+/*var input =document.getElementById('input_id');
 var button = document.getElementById('btn_id');
-var text = document.getElementById('teksti_id');
+var text=document.getElementById('teksti_id');
 
 button.onclick=function(){
-    text.innerHTML=input.value;
+    text.innerHTML=input.value ;
 }*/
 
-var text="the best school in the world is digital school!";
-var result=text.search("digital school");
-document.getElementById("result1").innerHTML=result;
+/*var text="The best school in the world is Digital School!";
+var result=text.search("Digital School");
+document.getElementById("result1").innerHTML = result;
+*/
+
+/*var text="The best school in the world is Digital School!";
+var result=text.search(/Digital School/);
+document.getElementById("result2").innerHTML=result;
+*/
+
+/*var text="The best school in the world is digital school!";
+var result=text.replace(/digital school/,"another school");
+document.getElementById("result3").innerHTML=result;
+*/
+
+/*var text="abcdef";
+var regex=new RegExp('abc');
+document.getElementById("result4").innerHTML=regex.test(text);*/
+
+/*var text="my school is the best school in the world ";
+var regex=/school/g;
+document.getElementById("result5").innerHTML=text.match(regex);*/
+
+/*var text="Digital school is the best school in the world ";
+var regex=/i/g;
+document.getElementById("result6").innerHTML=text.match(regex);*/
+
+/*var text="Digital school is the best school in the world ";
+var regex=/[abc]/g;
+document.getElementById("result7").innerHTML=text.match(regex);
+*/
+
+/*var text="Digital school is in top 10 best schools in the world ";
+var regex=/[0-9]/g;
+document.getElementById("result7").innerHTML=text.match(regex);
+*/
+//var text="Digital school is in top 10 best schools in the world ";
+//var regex=/[0-9]/g;
+//document.getElementById("result8").innerHTML=text.match(regex);
+
+var text ="my school is the best school in the world";
+var regex=/(top|best|school)/g;
+document.getElementById("result9").innerHTML=text.match(regex);
