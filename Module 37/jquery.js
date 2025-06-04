035ea9c5a6a83420c88dfa13d8cf528bbca47249
ZanaHoxha <zanahoxha681@gmail.com>
2025-06-04 18:24:34 +0200

    djgwehjv

---
+++ Module 37/jquery.js
@@ -0,0 +1,17 @@
+//$('sort').sortable();
+//$(function(){
+  //  $("accordion").accordion();
+//});
+//$('#tabs').tabs();
+
+$(function(){
+    $('#accordion').accordion({
+        collapsible: true
+    });
+});
+
+$(function(){
+    $("#tabs").tabs({
+        collapsible: true
+    });
+});
