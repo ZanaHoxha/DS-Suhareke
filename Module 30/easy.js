de5b3060bbb36e3d091c42c140dd2573f2aad808
ZanaHoxha <zanahoxha681@gmail.com>
2025-06-09 19:20:07 +0200

    fgertg

--- Module 30/easy.js
+++ Module 30/easy.js
@@ -1,7 +1,7 @@
 document.getElementsByTagName(button).addEventListener('click', () =>{
     const name = document.getElementById('name').value;
     const age = document.getElementById('age').value;
-    alert("Hello"+ name+", you are"+ age+ "yeas old!"):
+    alert("Hello"+ name+", you are"+ age+ "yeas old!");
 
 });
 
