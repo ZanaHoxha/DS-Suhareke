85f4fd159b77323597f46f4e7e8a5fe38142435b
ZanaHoxha <zanahoxha681@gmail.com>
2025-06-09 18:30:16 +0200

    hsrrftsdfhrsd

---
+++ Module 29/medium.js
@@ -0,0 +1,13 @@
+var programmingLanguage=[1,2,3,4,5];
+
+programmingLanguage.push(6);
+console.log(programmingLanguage);
+
+programmingLanguage.shift(1);
+console.log(programmingLanguage);
+
+programmingLanguage.splice(1,1,8);
+console.log(programmingLanguage);
+
+
+
